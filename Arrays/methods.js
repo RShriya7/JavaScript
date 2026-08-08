1463eb7555f13ae5c930ff1b8f2ad6221bca069e
RShriya7 <roddashriya7@gmail.com>
2026-08-08 21:07:34 +0530

    New Topic: Arrays

---
+++ Arrays/methods.js
@@ -0,0 +1,52 @@
+function Allmethods()
+{
+    //mutating -- changing the original array
+    // non mutating -- doing the change in the copy of the original array and returning that new array.
+
+    //1. slice -- non mutating
+    //to extract the portion of an array
+    console.log("Slice Method:")
+    const arr=[1,2,3,4];
+    const res = arr.slice(1,3);
+    console.log(res);
+
+    //Edge cases:
+    console.log(arr.slice(-2));//gets last 2 elements
+    console.log(arr.slice(0));
+    console.log(arr.slice(5));
+
+    //2. Splice method -- mutating
+    //to add, delete and replace elements
+    const arr1=[1,2,3,4];
+    //remove
+    const remove = arr1.splice(1,1);
+    console.log("removed element:",remove);
+    console.log("array after we removed an element:",arr1);
+
+    //add
+    const add= arr1.splice(1,0,99);
+    console.log("removed element:", add);
+    console.log("after adding:", arr1);
+
+    //replace
+    const replace = arr1.splice(2,1,50);
+    console.log("replaced element",replace);
+    console.log("after replacing:",arr1);
+
+    //3.concat -- to merge two arrays
+    const res1=[1,2,3];
+    const res2=[4,5,6];
+
+    const result = res1.concat(res2);
+    console.log(result);
+
+    //4. includes() -- checks if an element exists or not
+    console.log(result.includes(8));
+    console.log(result.includes(2));
+
+    //5. indexOf() -- tells the index of the element
+    console.log(result.indexOf(2));
+    console.log(result.indexOf(9));
+}
+
+Allmethods();
--- PJT1_5Days/PJT-1:smartCounterApp.js
+++ PJT1_5Days/PJT-1:smartCounterApp.js
@@ -1,4 +1,4 @@
-/*WITHOUT USING ANY 'THIS' 
+//SCENARIO -1: WITHOUT USING ANY 'THIS' 
 
 function CounterApp()
 {
@@ -14,13 +14,14 @@ function CounterApp()
     }
 }
 
+console.log("Scenario -1 Output");
 const result1 = CounterApp();
 console.log(result1.increment());
 console.log(result1.get());
 const result2=CounterApp();
 console.log(result2.decrement());
 console.log(result2.get());
-console.log(result2.incrementBy(2));*/
+console.log(result2.incrementBy(2));
 
 
 //SCENARIO-2: INTRODUCE THIS , BUT USE NORMAL FUNCTIONS
@@ -42,5 +43,42 @@ function createApp()
 }
 
 const result = createApp();
+console.log("Scenario -2 Output");
 console.log(result.increment());
 
+//SCENARION-3: USE THIS WITH AArrow functions
+
+function createCounterApp()
+{
+    return{
+        counter:0,
+
+        increment : ()=>
+        {
+            return ++this.counter;
+        }
+    }
+}
+console.log("Scenario -3 Output");
+const counter= createCounterApp();
+console.log(counter.increment());
+
+
+//SCENARIO -4: SOLUTION TO SCENARIO 3:- Asynchromous + this
+
+function createCounterApp() {
+    return {
+        counter: 0,
+
+        increment: function () {
+            setTimeout(() => {
+                console.log(++this.counter);
+            }, 1000);
+        }
+    };
+}
+
+console.log("Scenario -4 Output");
+const counter1 = createCounterApp();
+
+counter1.increment();
