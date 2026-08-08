5bd9acdaed485fae99b2468ae626857c9a8f3320
RShriya7 <roddashriya7@gmail.com>
2026-08-08 12:30:24 +0530

    Introducing this usage with normal functions

--- PJT1_5Days/PJT-1:smartCounterApp.js
+++ PJT1_5Days/PJT-1:smartCounterApp.js
@@ -1,3 +1,5 @@
+/*WITHOUT USING ANY 'THIS' 
+
 function CounterApp()
 {
     let counter=0;
@@ -18,4 +20,27 @@ console.log(result1.get());
 const result2=CounterApp();
 console.log(result2.decrement());
 console.log(result2.get());
-console.log(result2.incrementBy(2));
+console.log(result2.incrementBy(2));*/
+
+
+//SCENARIO-2: INTRODUCE THIS , BUT USE NORMAL FUNCTIONS
+function createApp()
+{
+    return{
+        counter:0,
+       increment:  function()
+        {
+            console.log(this);// to show what is the value of this.
+            return ++this.counter;
+        },
+
+        decrement: function(){
+            return --this.counter;
+        }
+
+    }
+}
+
+const result = createApp();
+console.log(result.increment());
+
